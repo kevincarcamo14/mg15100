7420d1dec620432c3e22ce0c526c5ff70dc73a6e
kevincarcamo14 <37493490+kevincarcamo14@users.noreply.github.com>
2018-10-02 10:01:40 -0600

    Add files via upload

---
+++ hola.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+
+int a[3][3];
+
+int filas, columnas;
+
+//Llenar la matriz
+
+for(filas=0; filas<3; filas++){
+	for(columnas=0; columnas<3; columnas++){
+		printf("Ingrese un valor: ");
+		scanf("%d", &a[filas][columnas]);
+		}
+}
+for(filas=0; filas<3; filas++){
+	printf("\n");
+	for(columnas=0; columnas<3; columnas++){
+	printf("%d\t", a[filas][columnas]);
+	}
+  }// metodo de la burbuja
+         for(int i=0; i<3; i++){
+	  printf("\n");
+	     for(int j=0; j<3; j++){
+	         printf("\n");                                                                                                                   
+     }     
+
